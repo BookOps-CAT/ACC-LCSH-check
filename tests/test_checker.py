2988686a64ff4f9de338952e7e44ebcdfb2f3437
Charlotte Kostelic <charlottekostelic@gmail.com>
2024-04-03 17:11:34 -0400

    unit-test fixes

--- tests/test_checker.py
+++ tests/test_checker.py
@@ -1,5 +1,6 @@
 import datetime
 import os
+import pytest
 
 from acc_lcsh_check.checker import read_data, get_data
 
@@ -11,6 +12,7 @@ def test_read_data():
         assert term[1].strip('" ') == "sh00000000"
 
 
+@pytest.mark.webtest
 def test_get_data_deprecated(mock_deprecated_skos_json_response):
     today = datetime.datetime.strftime(datetime.datetime.now(), "%Y%m%d")
     if os.path.exists(f"temp/deprecated_terms_{today}.txt"):
@@ -20,6 +22,7 @@ def test_get_data_deprecated(mock_deprecated_skos_json_response):
     assert outfile.read() == "sh00000000\n"
 
 
+@pytest.mark.webtest
 def test_get_data_changed(mock_current_skos_json_response):
     today = datetime.datetime.strftime(datetime.datetime.now(), "%Y%m%d")
     if os.path.exists(f"temp/changed_terms_{today}.txt"):
