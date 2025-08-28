10b4985b68aeb038826557d1cf3d13ece40d99df
Charlotte Kostelic <charlottekostelic@gmail.com>
2025-08-28 12:09:09 -0400

    clarified variable names

--- backstage_deletes.py
+++ backstage_deletes.py
@@ -25,9 +25,9 @@ def get_lc_results(row) -> list:
     return out
 
 
-def run(file: str, start: str = "0") -> None:
+def run(file: str, first_rec: str = "0") -> None:
     df = process_csv(file)
-    batches = get_batches(df, int(start))
+    batches = get_batches(df, int(first_rec))
     columns = [
         "RECORD_NUMBER",
         "LCCN",
@@ -40,19 +40,17 @@ def run(file: str, start: str = "0") -> None:
 
     for part in batches:
         chunk = df.loc[part[0] : part[1]]  # noqa: E203
-        start_time = time.time()
+        start = time.time()
         out = pd.DataFrame(data=None, columns=columns)
         out = chunk.apply(get_lc_results, axis=1).apply(pd.Series)
 
         out["COUNT"] = out.index.to_series().apply(lambda x: f"{x} of {len(df)}")
         record_count = out.pop("COUNT")
         out.insert(0, "COUNT", record_count)
-        end_time = time.time()
+        end = time.time()
         df.fillna("", inplace=True)
         write_delete_sheet(out, "1ljT9VxzdhuKHuYp9MhfOLcPxqfF6VgstKMSQgRELm-M")
-        print(
-            f"Records {part[0]} to {part[1]} took {end_time - start_time} seconds to run."
-        )
+        print(f"Records {part[0]} to {part[1]} took {end - start} seconds to run.")
 
 
 if __name__ in "__main__":
